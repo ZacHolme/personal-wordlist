67e45ead5cae0e4c92b7ea69df44cbe610e5b254
unknown <zholme3@gmail.com>
2022-02-12 12:18:33 +0000

    main script file added

---
+++ main.py
@@ -0,0 +1,30 @@
+# Used for command line
+import sys
+import string
+
+'''
+Created by Zach Holme, Luke Willimott and Nathan Brown
+personalWordlist v0.1
+'''
+
+
+def profile() -> string:
+    """ This might as well be a class but for now I've just done a function boilerplate """
+    ...
+    return 0
+
+
+def gen_list(user) -> string:
+    # function for generating wordlist
+    ...
+    return 0
+
+
+def main() -> string:
+    ...
+    gen_list(profile())
+    return 0
+
+
+if __name__ == '__main__':
+    main()
