21b8598fafabbe5723824a1bcaa615328962d1ca
unknown <zholme3@gmail.com>
2022-02-14 21:03:46 +0000

    Changed function to Profile method and added wordlist selection

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import sys
 import string
 
 '''
+--------------------------------------------------------
 Created by Zach Holme, Luke Willimott and Nathan Brown
 personalWordlist v0.1
+--------------------------------------------------------
 '''
 
 
@@ -13,25 +15,25 @@ class Profile:
     # Known personal info
     firstname = str
     lastname = str
+    dob = int
     house_number = int
-    address = str
-    # This could include any other unique data collected.
+    city = str
+    # This could include any other unique data collected. e.g. dog name, or unique id or code
     additonal_info = str
 
+    def create(self):
+        """Creates users profile instance"""
+        print('Victims Details: ')
+        self.firstname = input('First Name: ')
+        self.lastname = input('Last Name: ')
+        self.dob = input('Date of Birth (DD/MM/YYYY): ')
+        self.house_number = input('House Number: ')
+        self.city = input('City: ')
+        self.additonal_info = input('Any other additional information: (E.g. dog name) ')
+        return self
 
-def create_profile() -> string:
-    """ This might as well be a class but for now I've just done a function boilerplate """
-    new_profile = Profile()
-    new_profile.firstname = input('First Name: ')
-    new_profile.lastname = input('Last Name: ')
-    new_profile.house_number = input('House Number: ')
-    new_profile.address = input('Rest of Address (e.g. Preston Road)')
-    new_profile.additonal_info = input('Any other additional information: ')
-    return new_profile
 
-
-
-def gen_list(user: string) -> string:
+def gen_list(profile: Profile) -> string:
     # function for generating wordlist
     ...
     return 0
@@ -39,9 +41,23 @@ def gen_list(user: string) -> string:
 
 def main() -> string:
     ...
-    # gen_list(profile())
-    profile = create_profile()
-    print(profile.firstname)
+    new_profile: Profile = Profile()
+    Profile.create(new_profile)
+    checker = False
+    """Checks for false input and gives a wordlist selection for the user"""
+    while not checker:
+        wordlist = input(
+            f'(1) - Short wordlist\n(2) - Medium wordlist\n(3) - Long wordlist\nPick one of the following: ')
+        if wordlist == '1':
+            checker = True
+        elif wordlist == '2':
+            checker = True
+        elif wordlist == '3':
+            checker = True
+        else:
+            print('Incorrect input')
+            checker = False
+
     return 0
 
 
